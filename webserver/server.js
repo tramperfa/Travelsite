e655d49266e3dbc06e3cb00897c661a9c4a44fca
Boyang Zhang <boyang1988@gmail.com>
2017-08-01 09:02:35 -0500

    three containers working

--- webserver/server.js
+++ webserver/server.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var app = express();
+var MongoClient = require('mongodb').MongoClient;
+
+
+MongoClient.connect('mongodb://mongodb:27017', function (err, db) {
+  if (err) throw err
+  console.log('Connected to MongoDB at port 27017!');
+})
+
 
 app.get('/', function(req, res){
-  res.send("Hello Boyang, Start coding");
+  res.send("Hello Boyang, Start Coding Now!");
 });
 
 app.listen(8080, function(){
