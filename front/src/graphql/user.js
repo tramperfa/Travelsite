0c259a16a3999cb0cd5e813a854391ebcbb85674
Boyang Zhang <boyang1988@gmail.com>
2017-12-04 00:32:23 -0600

    updates

--- front/src/graphql/user.js
+++ front/src/graphql/user.js
@@ -1,5 +1,6 @@
 import gql from 'graphql-tag';
 
+////// QUERY
 export const meQuery = gql `
   query meQuery {
     me {
@@ -9,26 +10,6 @@ export const meQuery = gql `
     }
   }
 `;
-
-export const LoginMutation = gql `
-  mutation localLogin($input: localLoginInput!) {
-    localLogin(input: $input) {
-      me {
-          fullName
-          _id
-          #avatar
-      }
-    }
-  }
- `;
-
-export const LogoutMutation = gql `
- mutation logout($name: String){
-   logout(fullName: $name) {
-     success
-   }
- }
- `;
 
 export const userDetailQuery = gql `
    query userDetailsQuery($_id : ID!) {
@@ -41,6 +22,28 @@ export const userDetailQuery = gql `
      }
    }
  `;
+
+////// MUTATION
+
+export const LoginMutation = gql `
+   mutation localLogin($input: localLoginInput!) {
+     localLogin(input: $input) {
+       me {
+           fullName
+           _id
+           #avatar
+       }
+     }
+   }
+  `;
+
+export const LogoutMutation = gql `
+  mutation logout($name: String){
+    logout(fullName: $name) {
+      success
+    }
+  }
+  `;
 
 export const RegisterUserMutation = gql `
    mutation registerUser($input: registerUserInput!) {
