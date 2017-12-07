7ff2246acc6d10376d7fd922a2ea6cd562f56139
Boyang Zhang <boyang1988@gmail.com>
2017-12-06 18:53:18 -0600

    fixed login/register bug

--- front/src/components/StoryHeadline.js
+++ front/src/components/StoryHeadline.js
@@ -19,194 +19,192 @@ import willExtractOrientation from "./StoryEditor/util/ExtractOrientation"
 import willExtractSize from '../components/StoryEditor/util/ExtractSize';
 
 class StoryHeadline extends Component {
-  state = {
-    cropBox: {},
-    cropOpen: false, // Control Headline Image Cropper Open/Close
-    // Temp crop image always starting from null, can only be set by
-    tempCropImage: null
-  }
+	state = {
+		cropBox: {},
+		cropOpen: false, // Control Headline Image Cropper Open/Close
+		// Temp crop image always starting from null, can only be set by
+		tempCropImage: null
+	}
 
-  // Upload New Crop Image, Set tempCropImage
-  uploadFile = async (file) => {
-    try {
-      // {orientation: 8, src: <localURL>}
-      let localImageData = await willExtractOrientation(file)
-      let imageSize = await willExtractSize(localImageData)
-      let draftID = this.props.match.params._id
-      // Catergory: 1 HeadlineImage
-      let image = await willUploadImage(
-        file,
-        1,
-        draftID,
-        imageSize.width,
-        imageSize.height
-      )
-      console.log(image);
-      this.handleOpenCropper(image)
-    } catch (e) {
-      console.log("Headline Image Upload Error: " + e);
-    } finally {}
-  }
+	// Upload New Crop Image, Set tempCropImage
+	uploadFile = async (file) => {
+		try {
+			// {orientation: 8, src: <localURL>}
+			let localImageData = await willExtractOrientation(file)
+			let imageSize = await willExtractSize(localImageData)
+			let draftID = this.props.match.params._id
+			// Catergory: 1 HeadlineImage
+			let image = await willUploadImage(
+				file,
+				1,
+				draftID,
+				imageSize.width,
+				imageSize.height
+			)
+			console.log(image);
+			this.handleOpenCropper(image)
+		} catch (e) {
+			console.log("Headline Image Upload Error: " + e);
+		} finally {}
+	}
 
-  updateCropBox = (x, y, width, height) => {
-    this.setState({
-      cropBox: {
-        x: Math.round(x),
-        y: Math.round(y),
-        width: Math.max(Math.round(width), 900),
-        height: Math.max(Math.round(height), 300)
-      }
-    })
-  }
+	updateCropBox = (x, y, width, height) => {
+		this.setState({
+			cropBox: {
+				x: Math.round(x),
+				y: Math.round(y),
+				width: Math.max(Math.round(width), 900),
+				height: Math.max(Math.round(height), 300)
+			}
+		})
+	}
 
-  handleSubmit = async () => {
-    try {
-      let {x, y, width, height} = this.state.cropBox
-      let {cropImage} = this.props
-      // console.log("SUBMIT"); console.log(this.state.tempCropImage);
-      cropImage(this.state.tempCropImage._id, x, y, width, height)
-    } catch (e) {
-      console.log(e);
-    } finally {
-      this.handleCloseCropper()
-    }
-  }
-  handleCloseCropper = () => {
-    this.setState({tempCropImage: null, cropOpen: false});
-  }
-  handleOpenCropper = (tempCropImage) => {
-    this.setState({tempCropImage: tempCropImage, cropOpen: true})
-  }
-  onKeyPress = (event) => {
-    if (event.charCode === 13) { // enter key pressed
-      event.preventDefault()
-      this.handleSubmit()
-    }
-  }
+	handleSubmit = async () => {
+		try {
+			let {x, y, width, height} = this.state.cropBox
+			let {cropImage} = this.props
+			// console.log("SUBMIT"); console.log(this.state.tempCropImage);
+			cropImage(this.state.tempCropImage._id, x, y, width, height)
+		} catch (e) {
+			console.log(e);
+		} finally {
+			this.handleCloseCropper()
+		}
+	}
+	handleCloseCropper = () => {
+		this.setState({tempCropImage: null, cropOpen: false});
+	}
+	handleOpenCropper = (tempCropImage) => {
+		this.setState({tempCropImage: tempCropImage, cropOpen: true})
+	}
+	onKeyPress = (event) => {
+		if (event.charCode === 13) { // enter key pressed
+			event.preventDefault()
+			this.handleSubmit()
+		}
+	}
 
-  render() {
-    const {cropBox, cropOpen, tempCropImage} = this.state
-    const {headlineImage} = this.props.draft
-    // console.log(this.props.draft); console.log(headlineImage);
+	render() {
+		const {cropBox, cropOpen, tempCropImage} = this.state
+		const {headlineImage} = this.props.draft
+		// console.log(this.props.draft); console.log(headlineImage);
 
-    return (
-      <div>
-        {
-          tempCropImage && tempCropImage.originalImage && <Dialog
-              open={cropOpen}
-              transition={Slide}
-              onRequestClose={this.handleCloseCropper}
-              onKeyPress={this.onKeyPress}>
-              <DialogContent>
-                <ImageCrop
-                  updateCropBox={this.updateCropBox}
-                  minCropWidthOnCanvas={120}
-                  src={'https://s3.amazonaws.com/thetripbeyond/' + tempCropImage.originalImage.filename}/>
-              </DialogContent>
-              <div>
-                Current Croping Area: {cropBox.width}
-                X {cropBox.height}
-              </div>
-              <DialogActions>
-                <Button color="primary" onClick={this.handleSubmit}>
-                  Submit
-                </Button>
-                <Button color="primary" onClick={this.handleCloseCropper}>
-                  Cancel
-                </Button>
-              </DialogActions>
-            </Dialog>
-        }
-        <div>
-          {
-            (headlineImage && headlineImage.browserHeadlineImage)
-              ? <Headline
-                  headlineImage={headlineImage}
-                  uploadFile={this.uploadFile}
-                  handleOpenCropper={this.handleOpenCropper}/>
-              : <EmptyHeadline uploadFile={this.uploadFile}/>
-          }
-        </div>
-      </div>
-    );
-  }
+		return (
+			<div>
+				{
+					tempCropImage && tempCropImage.originalImage && <Dialog
+							open={cropOpen}
+							transition={Slide}
+							onRequestClose={this.handleCloseCropper}
+							onKeyPress={this.onKeyPress}>
+							<DialogContent>
+								<ImageCrop
+									updateCropBox={this.updateCropBox}
+									minCropWidthOnCanvas={120}
+									src={'https://s3.amazonaws.com/thetripbeyond/' + tempCropImage.originalImage.filename}/>
+							</DialogContent>
+							<div>
+								Current Croping Area: {cropBox.width}
+								X {cropBox.height}
+							</div>
+							<DialogActions>
+								<Button color="primary" onClick={this.handleSubmit}>
+									Submit
+								</Button>
+								<Button color="primary" onClick={this.handleCloseCropper}>
+									Cancel
+								</Button>
+							</DialogActions>
+						</Dialog>
+				}
+				<div>
+					{
+						(headlineImage && headlineImage.browserHeadlineImage)
+							? <Headline
+									headlineImage={headlineImage}
+									uploadFile={this.uploadFile}
+									handleOpenCropper={this.handleOpenCropper}/>
+							: <EmptyHeadline uploadFile={this.uploadFile}/>
+					}
+				</div>
+			</div>
+		);
+	}
 }
 
 StoryHeadline.propTypes = {
-  draft: PropTypes.object.isRequired,
-  cropImage: PropTypes.func.isRequired
+	draft: PropTypes.object.isRequired,
+	cropImage: PropTypes.func.isRequired
 }
 
 const Headline = (props) => {
-  var imageSource = 'https://s3.amazonaws.com/thetripbeyond/' + props.headlineImage.browserHeadlineImage.filename
-  var headlineImage = props.headlineImage
-  return (
-    <div>
-      <img className="headlineImage" src={imageSource} alt='headline'/>
-      <div>
-        <ImageInsert
-          uploadFile={props.uploadFile}
-          comment='Upload A Different Headline Image'/>
-      </div>
-      <div>
-        Edit Current Headline Image
-        <IconButton
-          aria-label="Edit"
-          onClick={() => props.handleOpenCropper(headlineImage)}>
-          <Edit/>
-        </IconButton>
-      </div>
-    </div>
-  )
+	var imageSource = 'https://s3.amazonaws.com/thetripbeyond/' + props.headlineImage.browserHeadlineImage.filename
+	var headlineImage = props.headlineImage
+	return (
+		<div>
+			<img className="headlineImage" src={imageSource} alt='headline'/>
+			<div>
+				<ImageInsert uploadFile={props.uploadFile} comment='Upload New Headline Image'/>
+			</div>
+			<div>
+				Edit Current Headline Image
+				<IconButton
+					aria-label="Edit"
+					onClick={() => props.handleOpenCropper(headlineImage)}>
+					<Edit/>
+				</IconButton>
+			</div>
+		</div>
+	)
 }
 
 const EmptyHeadline = (props) => {
-  return (
-    <div className="headline">
-      <div>
-        Suggest Use Original Image or Image Higher than 1980px
-        <ImageInsert uploadFile={props.uploadFile} comment='Upload Headline Image'/>
-      </div>
-    </div>
-  )
+	return (
+		<div className="headline">
+			<div>
+				Suggest Use Original Image or Image Higher than 1980px
+				<ImageInsert uploadFile={props.uploadFile} comment='Upload Headline Image'/>
+			</div>
+		</div>
+	)
 }
 
 export const WithCropImageMutation = graphql(CropImageMutation, {
-  props: ({mutate}) => ({
-    cropImage: (imageID, x, y, width, heigth) => mutate({
-      variables: {
-        input: {
-          imageID: imageID,
-          x: x,
-          y: y,
-          width: width,
-          height: heigth
-        }
-      },
-      update: (store, {data: {
-          cropImage
-        }}) => {
-        // Read the data from the cache for this query.
-        const data = store.readQuery({
-          query: DraftDetailsQuery,
-          variables: {
-            draftID: cropImage.draft
-          }
-        });
-        // change headline image.
-        data.draft.headlineImage = cropImage
-        // Write the data back to the cache.
-        store.writeQuery({
-          query: DraftDetailsQuery,
-          variables: {
-            draftID: cropImage.draft
-          },
-          data
-        });
+	props: ({mutate}) => ({
+		cropImage: (imageID, x, y, width, heigth) => mutate({
+			variables: {
+				input: {
+					imageID: imageID,
+					x: x,
+					y: y,
+					width: width,
+					height: heigth
+				}
+			},
+			update: (store, {data: {
+					cropImage
+				}}) => {
+				// Read the data from the cache for this query.
+				const data = store.readQuery({
+					query: DraftDetailsQuery,
+					variables: {
+						draftID: cropImage.draft
+					}
+				});
+				// change headline image.
+				data.draft.headlineImage = cropImage
+				// Write the data back to the cache.
+				store.writeQuery({
+					query: DraftDetailsQuery,
+					variables: {
+						draftID: cropImage.draft
+					},
+					data
+				});
 
-      }
-    })
-  })
+			}
+		})
+	})
 })
 
 export default WithCropImageMutation(StoryHeadline)
--- webserver/src/auth/passport.js
+++ webserver/src/auth/passport.js
@@ -1,44 +1,38 @@
-
-
 /*!
  * Module dependencies.
  */
 
 import local from './strategy/local';
-//import google from './passport/google';
-//import facebook from './passport/facebook';
+// import google from './passport/google'; import facebook from
+// './passport/facebook';
 import passport from 'passport';
 
-module.exports = function(app) {
+module.exports = function (app) {
 
+	passport.serializeUser(function (user, done) {
 
-  passport.serializeUser(function(user, done) {
+		console.log("reach serialize user step")
+		// console.log("serializing user :  " + user ); Check that the user is not
+		// disabled or deleted if (user.status !== 1) done(null, false);
 
-    console.log("reach serialize user step")
-    //console.log("serializing user :  " + user );
+		var sessionUser = {
+			_id: user._id,
+			role: user.role
+		}
+		done(null, sessionUser)
+	});
 
-    // Check that the user is not disabled or deleted
-    // if (user.status !== 1)
-    // done(null, false);
+	passport.deserializeUser(function (sessionUser, done) {
+		console.log("reach deserialize user step")
+		// The sessionUser object is different from the user mongoose collection it's
+		// actually req.session.passport.user and comes from the session collection
+		done(null, sessionUser);
+	});
 
-    var sessionUser = { _id: user._id, role: user.role }
-    done(null, sessionUser)
-  });
-
-  passport.deserializeUser(function(sessionUser, done) {
-      console.log("reach deserialize user step")
-    // The sessionUser object is different from the user mongoose collection
-    // it's actually req.session.passport.user and comes from the session collection
-    done(null, sessionUser);
-  });
-
-  // use these strategies
-  passport.use(local);
-  //passport.use(google);
-  //passport.use(facebook);
-
-  // use passport session
-  app.use(passport.initialize());
-  app.use(passport.session());
+	// use these strategies
+	passport.use(local);
+	//passport.use(google); passport.use(facebook); use passport session
+	app.use(passport.initialize());
+	app.use(passport.session());
 
 };
