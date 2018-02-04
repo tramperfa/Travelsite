bc46f6f4f6d2fd24725452d588797b3084d9b8ac
Boyang Zhang <boyang1988@gmail.com>
2018-02-04 16:26:12 -0600

    Finished Code splitting for now

--- front/src/components/Loading.js
+++ front/src/components/Loading.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
-const Loading = ({bundleName}) => {
-	return (
-		//
-		<div>
-			Loading {bundleName}
-		</div>
-	)
+const Loading = (props) => {
+
+	if (props.error) {
+		return <div>Error!</div>;
+	} else if (props.pastDelay) {
+		return <div>Loading GO!!!!!!!</div>;
+	} else {
+		return null;
+	}
 }
 
 export default Loading
